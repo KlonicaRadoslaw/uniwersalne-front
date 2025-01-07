e165c00c9f115894601194061a0b8ec737ae7857
Sylwia Krzysztoń <sylwia.krzy64@gmail.com>
2025-01-07 20:42:38 +0100

    feat: update data in database

--- src/helperFunctions/loadGamesData.jsx
+++ src/helperFunctions/loadGamesData.jsx
@@ -1,10 +1,11 @@
 export const loadGamesData = async () => {
   try {
-    const response = await fetch('gamesDatabase.json');
+    const response = await fetch('/gamesDatabase.json');
     if (!response.ok) {
       throw new Error('Failed to fetch games data');
     }
     const data = await response.json();
+    console.log("Raw",data.games);
     return data.games;
   } catch (error) {
     console.error('Error loading games data:', error);
